174f9cb9df040004c896a3ed6d8ccbbe98311d85
chelik10228 <dxxeas@gmail.com>
2025-08-10 22:43:17 +0200

    removed build in c and made in script

--- cpu/build.c
+++
@@ -1,9 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-
-main() {
-  puts("\033[32mrebuilding\033[0m lxc8");
-  system("gcc cpu/cpu.c -o lxc8 --std=c89");
-  puts("\033[32mrebuilding\033[0m build");
-  system("gcc cpu/build.c -o build --std=c89");
-}
